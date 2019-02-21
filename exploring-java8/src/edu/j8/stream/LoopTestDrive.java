9aef61481f78139277fbbb0b2c0ae4aa3c36ee0a
264030 <264030@PC399852.cts.com>
2019-02-21 10:27:41 +0530

    Committed for infinite loop

---
+++ exploring-java8/src/edu/j8/stream/LoopTestDrive.java
@@ -0,0 +1,12 @@
+package edu.j8.stream;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class LoopTestDrive {
+
+	public static void main(String[] args) {
+		Stream.iterate(1, i -> i + 1).limit(100).peek(System.out::println).collect(Collectors.toList());
+		System.out.println("End");
+	}
+}
